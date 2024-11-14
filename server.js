f799277dbb710c19984c5ee1b9ba82c1d3e903e0
ziynatilloh-html <ziynatillokht@gmail.com>
2024-11-14 21:41:49 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 console.log("Web serverni boshlash");
 const express = require("express");
-
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const { parse } = require("path");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    (user = JSON), parse(data);
+  }
+});
 
 //1 //expressga kirib kelayotgan ma`lumotlarga bog`liq kodlar yoziladi! // KIRISH CODE
 app.use(express.static("public"));
@@ -19,14 +30,13 @@ app.set("view engine", "ejs");
 //4 ROUTING CODE
 app.post("/create-item", (req, res) => {
   console.log(req.body);
-  res.json({ test: "Success " });
 });
 app.get("/", function (req, res) {
   res.render("harid");
 });
-// app.get("/hello", function (req, res) {
-//   res.end("<h1>Ha ishlaganiga hursand bo`lib o`tiribsanmi</h1>");
-// });
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
 // app.get("/gift", function (req, res) {
 //   res.end("<h1>IT is not over until i win</h1>");
 // });
