59ac6ea95cba330732b8cef83cdcd7d237e89683
ziynatilloh-html <ziynatillokht@gmail.com>
2024-11-24 14:50:59 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -28,15 +28,11 @@ app.set("view engine", "ejs");
 // Routing
 app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
-  console.log(req.body);
+  // console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    // console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
---
+++ public/browser.js
@@ -0,0 +1,40 @@
+console.log(" Front  JS ishga tushdi ");
+function itemTemplate(item) {
+  return ` <li
+            style="margin-top: 20px; border-radius: 40px"
+            class="list-group-item list-group-item-info d-flex align-items-center justify-content-between"
+          >
+            <span class="item-text">${item.reja}</span>
+            <div>
+              <button
+                data-id="${item._id}"
+                class="edit-me btn btn-secondary btn-sm mr-1"
+              >
+                Õzgartirish
+              </button>
+              <button
+                data-id="${item._id}"
+                class="delete-me btn btn-danger btn-sm"
+              >
+                Õchirish
+              </button>
+            </div>
+          </li>`;
+}
+let createField = document.getElementById("create-field");
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  axios
+    .post("/create-item", { reja: createField.value })
+    .then((response) => {
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+      createField.value = "";
+      createField.focus();
+    })
+    .catch((err) => {
+      console.log("Nimadir xato ketdi, qaytadan urunib ko`ring 🫶🏻", err);
+    });
+});
