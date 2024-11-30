2d6fd417b5d5b24efc9ff0255c5ee01c22119c61
ziynatilloh-html <ziynatillokht@gmail.com>
2024-11-30 15:31:46 +0900

    feat:

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongodb.connect(
       module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
-      const PORT = 3000;
+      const PORT = 5005;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
